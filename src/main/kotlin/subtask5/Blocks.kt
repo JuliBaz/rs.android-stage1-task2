1e02782614ebb09b84779f1933633a9fc66bb43e
JuliBaz <61162307+JuliBaz@users.noreply.github.com>
2020-04-14 22:23:35 +0300

    task1-2

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,47 @@
 package subtask5
 
 import kotlin.reflect.KClass
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when (blockB) {
+            Int::class -> {
+                var sum = 0
+                for (i in blockA) {
+                    if (i is Int) {
+                        sum += i
+                    }
+                }
+                return sum
+            }
+
+            String::class -> {
+                var str = StringBuilder()
+                for (i in blockA) {
+                    if (i is String) {
+                        str.append(i)
+                    }
+                }
+                return str.toString()
+            }
+            else -> {
+                var data = LocalDate.now()
+                var flag = true
+                for (i in blockA) {
+                    if (i is LocalDate) {
+                        if ((flag) || (i > data)) {
+                            flag = false
+                            data = i
+                        }
+                    }
+                }
+                return data.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            }
+        }
     }
 }
+
