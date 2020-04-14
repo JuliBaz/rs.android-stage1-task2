8ebd862ce979d939fcd649481e4b624266685e88
JuliBaz <61162307+JuliBaz@users.noreply.github.com>
2020-04-14 21:38:42 +0300

    task1-2

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,26 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
-    }
-}
+        return try {
+            val data = GregorianCalendar()
+            data.isLenient = false
+            data.set(Calendar.DAY_OF_MONTH, day.toInt())
+            data.set(Calendar.MONTH, month.toInt()-1)
+            data.set(Calendar.YEAR, year.toInt())
+
+            val specialDateFormat = SimpleDateFormat("d MMMM, EEEE", Locale("ru"))
+            specialDateFormat.format(data.time)
+
+        }
+        catch (i: IllegalArgumentException){
+            "Такого дня не существует" }
+} }
+
